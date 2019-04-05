cc1f42396a428bfa78e39c9750fcba77f28b5c1d
xabiDw1b <xabier.revuelta@gmail.com>
2019-04-05 11:24:08 +0200

    Prueba

--- CalculadoraEnde/src/interfazCLI/com/Calculadora.java
+++ CalculadoraEnde/src/interfazCLI/com/Calculadora.java
@@ -11,6 +11,72 @@ package interfazCLI.com;
  * @author israel
  *
  */
-public class Calculadora_Potencias {
 
+public class Calculadora {
+
+	private float num1,num2;
+	
+	
+	public Calculadora(float num1, float num2)
+	{
+		
+		this.num1=Consola.leeFloat();
+		this.num2=Consola.leeFloat();
+		
+	}
+	
+	public float sumar(float num1, float num2)
+	{
+		float resul;
+		
+		resul=num1+num2;
+		
+		return resul;
+		
+	}
+	
+	
+	
+	public float dividir(float num1, float num2)
+	{
+		
+		try {
+			float resul;
+			
+			resul=num1/num2;
+			
+			
+			return resul;
+			
+		}catch(ArithmeticException e) {
+			
+			System.out.println("No se puede dividir entre 0");
+
+			return -1;
+		}
+
+		
+	}
+	
+	
+	public float exponente(float num1, float num2)
+	{
+		
+		float resul=0;
+		
+		for (int i=0;i<num2;i++)
+		{
+			resul=num1+resul;
+			
+		}
+
+		
+		return resul;
+		
+		
+		
+		
+	}
+	
+	
 }
